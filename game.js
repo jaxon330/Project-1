8c4546fa9ede37d6fb77408489363aceaa5116f2
john-330 <boss_jaxon@mail.ru>
2021-11-09 23:07:29 -0600

    Markup and styles of the game are created

---
+++ game.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Sounds effects
+green button
+https://s3.amazonaws.com/freecodecamp/simonSound1.mp3
+
+red button
+https://s3.amazonaws.com/freecodecamp/simonSound2.mp3
+
+blue button
+https://s3.amazonaws.com/freecodecamp/simonSound3.mp3
+
+yellow button
+https://s3.amazonaws.com/freecodecamp/simonSound4.mp3
+
+*/
