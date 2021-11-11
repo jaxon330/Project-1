55006464fc953ef9c4a69347fb0024ef90720387
john-330 <boss_jaxon@mail.ru>
2021-11-10 21:17:51 -0600

    Day 2. built functions which make a sounds and animates when button is pressed

--- game.js
+++ game.js
@@ -1,29 +1,36 @@
+const buttonColors = ['red', 'blue', 'green', 'yellow']
+let gamePattern = []
+let userClickedPattern = []
+
+
+function nextSequence() {
+   let randomNumber = Math.floor(Math.random()*3)
+   let randomChoosenColour = buttonColors[randomNumber]
+   gamePattern.push(randomChoosenColour)
+   $('#'+ randomChoosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChoosenColour)
+  
+}
+
+nextSequence()
+
+
+// User clicked pattern
+
+$('.btn').on('click', (e) => {
+    // console.log(e.target.id)
+    let userChoosenColor = e.target.id
+    userClickedPattern.push(userChoosenColor)
+    console.log(userClickedPattern)
+    playSound(userChoosenColor)
+})
+
+// how to play a sound in javascript?
+
+function playSound(index) {
+    var audio = new Audio('sounds/'+ index + '.mp3')
+    audio.play();
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-/* 
-Sounds effects
-green button
-https://s3.amazonaws.com/freecodecamp/simonSound1.mp3
-
-red button
-https://s3.amazonaws.com/freecodecamp/simonSound2.mp3
-
-blue button
-https://s3.amazonaws.com/freecodecamp/simonSound3.mp3
-
-yellow button
-https://s3.amazonaws.com/freecodecamp/simonSound4.mp3
-
-*/
