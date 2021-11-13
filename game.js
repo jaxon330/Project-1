e009bc45e2dcf64db47fab09c0d9905017cfc941
john-330 <boss_jaxon@mail.ru>
2021-11-12 20:23:06 -0600

    built functions when answer wrong and to restart the game

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 const buttonColors = ['red', 'blue', 'green', 'yellow']
 let gamePattern = []
 let userClickedPattern = []
@@ -6,7 +5,7 @@ let level = 0;
 let started = false;
 
 $(document).keypress(() => {
-    if (!started) {
+    if (started === false) {
         // $('.level').text(`Level ${level}`)
         nextSequence()
         started = true;
@@ -18,15 +17,15 @@ $(document).keypress(() => {
 
 // User clicked pattern
 
-$('.btn').click((e) =>  {
+$('.btn').click((e) => {
     // console.log(e.target.id)
     let userChoosenColor = e.target.id
     userClickedPattern.push(userChoosenColor)
     console.log(userClickedPattern.length)
     playSound(userChoosenColor)
     //
-    checkAnswer(userClickedPattern.length-1)
-    console.log('user '+userClickedPattern);
+    checkAnswer(userClickedPattern.length - 1)
+    console.log('user ' + userClickedPattern);
 })
 
 // game condition
@@ -34,30 +33,33 @@ function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
         console.log('success');
         // console.log('computer '+gamePattern);
-        console.log('user '+userClickedPattern);
+        console.log('user ' + userClickedPattern);
         // console.log(userClickedPattern.length)
         if (gamePattern.length === userClickedPattern.length) {
-            setTimeout(nextSequence(), 1000)
+            setTimeout(function () {
+                nextSequence()
+            }, 1000)
             userClickedPattern = []
         }
     } else {
-        console.log('wrong');
+        wrongSequence()
         // console.log('computer '+gamePattern);
         // console.log('user '+userClickedPattern);
-        $('body').css('background-color', 'red')
+
     }
 }
+
 
 function nextSequence() {
     level++;
     $('.level').text(`Level ${level}`)
-   let randomNumber = Math.floor(Math.random()*3)
-   let randomChoosenColour = buttonColors[randomNumber]
-   gamePattern.push(randomChoosenColour)
-   $('#'+ randomChoosenColour).fadeOut(100).fadeIn(100);
+    let randomNumber = Math.floor(Math.random() * 3)
+    let randomChoosenColour = buttonColors[randomNumber]
+    gamePattern.push(randomChoosenColour)
+    $('#' + randomChoosenColour).fadeOut(100).fadeIn(100);
     playSound(randomChoosenColour)
-    console.log('computer '+gamePattern);
-   
+    console.log('computer ' + gamePattern);
+
 }
 
 
@@ -65,8 +67,30 @@ function nextSequence() {
 // how to play a sound in javascript?
 
 function playSound(index) {
-    var audio = new Audio('sounds/'+ index + '.mp3')
+    var audio = new Audio('sounds/' + index + '.mp3')
     audio.play();
 }
 
+// game over
+function wrongSequence() {
+    console.log('wrong');
+    let audio = new Audio('sounds/error.wav')
+    audio.play()
+    $('body').addClass('red')
+    setTimeout(function () {
+        $('body').removeClass('red')
+    }, 300)
+    $('.restart').addClass('show')
+    // $('.btn').fadeOut(500)
+    $('.level').text('Game Over, Press Restart button to Restart')
+}
 
+// restart button
+$('.restart').click(() => {
+    gamePattern = []
+    userClickedPattern = []
+    level = 0;
+    $('.restart').removeClass('show')
+    nextSequence()
+
+})
